f247480f95e9cf13a1da2402a341e94b8c4b5491
lomar nfi <70724305+Gr2am@users.noreply.github.com>
2022-09-28 03:56:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+##
+
+class Vehical():
+    def __init__(self, bodystyle):
+      self.bodystyle = bodystyle 
+
+class car(Vehical):
+    def __init__(self, engine):
+      super().__init__("car")
+      self.wheels = 4
+      self.doors = 4
+      self.engine = engine
+
+car1 = car("gas")
+car2 = car("Electr")
+
+print(car1.engine)
+print(car2.doors)
